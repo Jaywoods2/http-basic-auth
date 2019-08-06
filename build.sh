873bb7bf23a9be9613ada15b73fd97db1b834529
宾建武 <jianwu.bin@hand-china.com>
2019-08-06 15:34:47 +0800

    fix

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build main.go
-
-mv main httpbasicauth
-
-docker build -t registry.cn-shenzhen.aliyuncs.com/jaywoods/httpbasicauth .
-
-docker push  registry.cn-shenzhen.aliyuncs.com/jaywoods/httpbasicauth:latest
